a6dd7a3f4adbc13c46b0f2a5048343bc28e7745e
ElectricGamer100 <MCAnonymousModders@gmail.com>
2015-07-28 09:19:02 -0400

    Update OvenRenderer.cpp
    Without the things I added the mod will crash

--- jni/Furniture/render/tile/renderers/OvenRenderer.cpp
+++ jni/Furniture/render/tile/renderers/OvenRenderer.cpp
@@ -9,13 +9,13 @@ void OvenRenderer::render(const TilePos& pos, FurnitureTile* tile, TileTessellat
 	tess->tex1(tess->getLightColor(pos));
 
 	tess->forcedUV = tile->getTextureUVCoordinateSet("quartz_block", 1);
-	this->setRotatedBounds(tess, data, 0, 0, 0.1, 1, 1, 0.9);
+	this->setRotatedBounds(tess, data, 0.0, .0, 0.1, 1.0, 1.0, 0.9);
 	tess->tessellateBlockInWorld(tile, {x, y, z});
-	this->setRotatedBounds(tess, data, 0.9, 1.0, 0.1, 1, 1.2, 0.9);
+	this->setRotatedBounds(tess, data, 0.9, 1.0, 0.1, 1.0, 1.2, 0.9);
 	tess->tessellateBlockInWorld(tile, {x, y, z});
 	
 	tess->forcedUV = tile->getTextureUVCoordinateSet("stone", 0);
-	this->setRotatedBounds(tess, data, -0.1, 0.1, 0.2, 0, 0.9, 0.8);
+	this->setRotatedBounds(tess, data, -0.1, 0.1, 0.2, 0.0, 0.9, 0.8);
 	tess->tessellateBlockInWorld(tile, {x, y, z});
 
 	tess->forcedUV = tile->getTextureUVCoordinateSet("obsidian", 0);
